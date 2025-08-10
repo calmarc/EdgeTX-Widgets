261781910b28e44d48a13c4c1e3d66f9f9214044
Calmar Bro <mac@calmar.ws>
2025-08-10 14:15:05 +0200

    licence and in Widget-Folder

--- WIDGETS/BattMeter/main.lua
+++ WIDGETS/BattMeter/main.lua
@@ -1,3 +1,10 @@
+-- LinkMeter Widget
+-- Copyright (C) 2025 Calari and chatgpt
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+
 local options = {
   { "tx_voltage", BOOL, 1 },
   { "Cells", VALUE, 3 },
--- WIDGETS/LinkMeter/main.lua
+++ WIDGETS/LinkMeter/main.lua
@@ -1,3 +1,10 @@
+-- LinkMeter Widget
+-- Copyright (C) 2025 Calari and chatgpt
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+
 local options = {
   { "ShowPercent", BOOL, 1 },                           -- Prozentwert anzeigen
   { "Text", COLOR, lcd.RGB(255, 255, 255) },            -- Textfarbe
